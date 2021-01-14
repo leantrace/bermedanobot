ddb35f9bd783b4c9f8c6e9cf4c78792f6edccfba
Alexander Schamne <alexander.schamne@gmail.com>
2021-01-14 13:28:08 +0000

    add jokes

--- src/main/kotlin/Joke.kt
+++ src/main/kotlin/Joke.kt
@@ -1,12 +1,9 @@
 class Joke(val text: String, val image: String? = null)
 
 val jokes = listOf(
-        Joke("Prediction:\nThere will be a minor baby boom in 9 months,\nand then one day in 2033, we shall whitness the rise of the QUARANTEENS."),
-        Joke("If you need 144 rolls of toilet paper for a 14 day quarantine,\nyou probably should have been seeing a Doctor long before COVID-19."),
-        Joke("The worst thing about corona?\nCOVID-19 looks like a Jira issue."),
-        Joke("What is Happiness?\nWhen you work at a bank and two guys with masks come in\nbut they're just robbing the place."),
-        Joke("Quarantine diary\n\nDay 1: I have stocked up on enough non-perishable food and supplies to last me for months, maybe years, so that I can remain in isolation for as long as it takes to see out this pandemic.\n\nDay 1 + 45 minutes: I am in the supermarket because I wanted a Twix."),
-        Joke("Kid: Hey mum, when is this corona virus thing gonna be over?\nMum: Just shut up and eat your toilet paper."),
-        Joke("Chuck Norris has been exposed to the Coronavirus.\nThe virus is now in quarantine for a month."),
-        Joke("Some people aren't shaking hands because of the Corona virus.\nI'm not shaking hands because everyone is out of toilet paper."),
+        Joke("Why did Elsa lose her kite? She let it go."),
+        Joke("Getting my kite stuck in a tree isn't my favorite thing...But it's up there."),
+        Joke("Which medication kites behind your boat? Parasailtamol!"),
+        Joke("Life is like a kite - sometimes you get zapped by lightning"),
+        Joke("LDid you hear about the kite that went up into the air really easily? - He said it was a breeze.")
 )
