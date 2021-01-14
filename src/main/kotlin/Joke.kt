f92d2b3db81c0472c8c96edbd3eab49c245da6ae
Alexander Schamne <alexander.schamne@gmail.com>
2021-01-14 13:55:51 +0000

    add jokes

--- src/main/kotlin/Joke.kt
+++ src/main/kotlin/Joke.kt
@@ -5,10 +5,5 @@ val jokes = listOf(
         Joke("Getting my kite stuck in a tree isn't my favorite thing...But it's up there."),
         Joke("Which medication kites behind your boat? Parasailtamol!"),
         Joke("Life is like a kite - sometimes you get zapped by lightning"),
-        Joke("Did you hear about the kite that went up into the air really easily? - He said it was a breeze."),
-        Joke("","kite_temtation.jpeg"),
-        Joke("","never_give_up.jpeg"),
-        Joke("","random1.jpeg"),
-        Joke("","sweets_temtation.jpeg"),
-        Joke("","wind1.jpeg")
+        Joke("Did you hear about the kite that went up into the air really easily? - He said it was a breeze.")
 )
