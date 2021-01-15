00d96d9fe8b714285adb8512136bef2475f9c2d5
wazest <stefan.waldner@spoud.io>
2021-01-15 13:01:00 +0000

    remove .

---
+++ target/classes/Emotion.kt
@@ -0,0 +1,2 @@
+val hates = listOf("kalt", "rocks", "zweni", "corona")
+val loves = listOf("yoga", "kite", "sand", "upwind").map { Regex(it) }
---
+++ target/classes/Quote.kt
@@ -0,0 +1,6 @@
+val quotes = listOf(
+        "Het öpper kiten gseit?",
+        "Isch dr Wind drum geil?",
+        "Jump, jump - Lern erstmal richtig Toeside fahren!",
+        "Man! Het dr Stibu schowieder gfurzt!?",
+)
