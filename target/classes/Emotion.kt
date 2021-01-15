84b2b8a881b2c6ff1b304e5b239d5c4f324a680e
wazest <stefan.waldner@spoud.io>
2021-01-15 12:56:47 +0000

    git ignore

--- target/classes/Emotion.kt
+++
@@ -1,2 +0,0 @@
-val hates = listOf("kalt", "rocks", "zweni", "corona")
-val loves = listOf("yoga", "kite", "sand", "upwind").map { Regex(it) }
--- target/classes/Meme.kt
+++
@@ -1,1 +0,0 @@
-val memes = listOf("kite_temptation.jpeg", "never_give_up.jpeg", "random1.jpeg", "sweets_temptation.jpeg", "wind1.jpeg")
