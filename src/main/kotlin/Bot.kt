904d78277f17d3431158cafe8301a744f969266d
wazest <stefan.waldner@spoud.io>
2021-01-15 20:45:21 +0000

    ecode 3

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -23,9 +23,9 @@ class Bot : TelegramLongPollingBot() {
     private val rnd = Random
     private val react = listOf("kite", "surf", "wind").map { Regex(it) }
     private val members = mapOf (
-        "alex" to listOf("Meinst du Alex den Kite-Wolverine?!", "JESUS IS COMING :U+1F44F:"),
+        "alex" to listOf("Meinst du Alex den Kite-Wolverine?!", "JESUS IS COMING 0xF00x9F0x9A0x80"),
         "stibu" to listOf("Yo dr Stibu chas drum o!", "Stibu? Dä wo immer mitem Kite umefailed?"),
-        "isa" to listOf("Die Isa? o.O Die Upwind-Isa?!?", "Die Isa? Hei Sie trinkt imfau ou mate! :U+1F4A5:")
+        "isa" to listOf("Die Isa? o.O Die Upwind-Isa?!?", "Die Isa? Hei Sie trinkt imfau ou mate!")
     )
 
     init {
