fca65273bea3a8801c5bf4c9ac6ee553b4bfecc8
wazest <stefan.waldner@spoud.io>
2021-01-15 13:06:46 +0000

    target?

--- target/classes/Joke.kt
+++
@@ -1,9 +0,0 @@
-class Joke(val text: String, val image: String? = null)
-
-val jokes = listOf(
-        Joke("Why did Elsa lose her kite? She let it go."),
-        Joke("Getting my kite stuck in a tree isn't my favorite thing...But it's up there."),
-        Joke("Which medication kites behind your boat? Parasailtamol!"),
-        Joke("Life is like a kite - sometimes you get zapped by lightning"),
-        Joke("Did you hear about the kite that went up into the air really easily? - He said it was a breeze.")
-)
--- target/classes/Quote.kt
+++
@@ -1,6 +0,0 @@
-val quotes = listOf(
-        "Het öpper kiten gseit?",
-        "Isch dr Wind drum geil?",
-        "Jump, jump - Lern erstmal richtig Toeside fahren!",
-        "Man! Het dr Stibu schowieder gfurzt!?",
-)
