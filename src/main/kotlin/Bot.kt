a99d4befb2e98667a68b81d8726b899cdae4ed1e
Alexander Schamne <alexander.schamne@gmail.com>
2021-01-24 14:01:03 +0000

    remove unused imports

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -1,10 +1,9 @@
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
 import io.ktor.client.features.json.*
-import io.ktor.client.request.*
 import io.ktor.client.request.forms.*
 import io.ktor.http.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.runBlocking
 import org.telegram.telegrambots.ApiContextInitializer
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.TelegramBotsApi
@@ -17,8 +16,6 @@ import java.io.OutputStreamWriter
 import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLEncoder
-import java.net.http.HttpResponse
-import kotlin.coroutines.CoroutineContext
 import kotlin.random.Random
 
 fun main() {
@@ -32,7 +29,7 @@ class Bot : TelegramLongPollingBot() {
 
     private val rnd = Random
     private val react = listOf("kite", "surf", "wind").map { Regex(it) }
-    private val members = mapOf (
+    private val members = mapOf(
         "alex" to listOf("Meinst du Alex den Kite-Wolverine?!", "JESUS IS COMING 0xF00x9F0x9A0x80"),
         "stibu" to listOf("Yo dr Stibu chas drum o!", "Stibu? Dä wo immer mitem Kite umefailed?"),
         "isa" to listOf("Die Isa? o.O Die Upwind-Isa?!?", "Die Isa? Hei Sie trinkt imfau ou mate!")
@@ -57,7 +54,7 @@ class Bot : TelegramLongPollingBot() {
     override fun getBotToken() = System.getenv("BOT_TOKEN")
     override fun getBotUsername() = System.getenv("BOT_USER")
 
-    fun sendPostRequest(chatId: Long, userName:String, password:String) {
+    fun sendPostRequest(chatId: Long, userName: String, password: String) {
 
         var reqParam = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(userName, "UTF-8")
         reqParam += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8")
@@ -100,16 +97,26 @@ class Bot : TelegramLongPollingBot() {
             fun send(text: String) = execute(SendMessage(chatId, text))
 
             // https://imgflip.com/api
-            data class Box(val text: String, val x: Int, val y: Int, val width: Int, val height: Int,
-                           val color: String, val outline_color: String)
-            data class ImageFlip(val template_id: String, val username: String, val password: String,
-                                 val text0: String, val text1: String, val font: String? = null,
-                                 val max_font_size: String? = null, val boxes: List<Box>? = null)
+            data class Box(
+                val text: String, val x: Int, val y: Int, val width: Int, val height: Int,
+                val color: String, val outline_color: String
+            )
+
+            data class ImageFlip(
+                val template_id: String, val username: String, val password: String,
+                val text0: String, val text1: String, val font: String? = null,
+                val max_font_size: String? = null, val boxes: List<Box>? = null
+            )
+
             data class ImageFlipResponseData(val url: String, val page_url: String)
-            data class ImageFlipResponse(val success: String, val data: ImageFlipResponseData? = null, val error_message: String? = null)
+            data class ImageFlipResponse(
+                val success: String,
+                val data: ImageFlipResponseData? = null,
+                val error_message: String? = null
+            )
 
             fun sendImage(name: String, caption: String) = execute(SendPhoto().apply {
-                println("Send Photo: "+name)
+                println("Send Photo: " + name)
                 setChatId(chatId)
                 setCaption(caption)
                 runBlocking {
@@ -125,9 +132,9 @@ class Bot : TelegramLongPollingBot() {
                             append("text1", "...Uff d..da..das tut mir leid")
                         })
                     send(response.success)
-                    send(response.error_message?:"")
-                    send(response.data?.url?:"")
-                    send(response.data?.page_url?:"")
+                    send(response.error_message ?: "")
+                    send(response.data?.url ?: "")
+                    send(response.data?.page_url ?: "")
                     client.close()
                 }
                 // setPhoto(name, URL("https://i.imgflip.com/22bdq6.jpg").openStream())
@@ -138,13 +145,13 @@ class Bot : TelegramLongPollingBot() {
                 setChatId(chatId)
                 setCaption(caption)
                 val m = if (mood != null) "/$mood" else ""
-                val c = if (caption != null) "/says/${caption.replace("cat\\s*".toRegex(),"")}" else ""
+                val c = if (caption != null) "/says/${caption.replace("cat\\s*".toRegex(), "")}" else ""
                 setPhoto("cat", URL("https://cataas.com/cat${m}${c}").openStream())
             })
 
             when {
                 text.startsWith("/help") -> send("Chatte einfach ganz normal, ich werd schon etwas sagen, wenn ich etwas zu sagen habe...")
-                listOf("cat").any { it in text } -> sendCatImage("",text)
+                listOf("cat").any { it in text } -> sendCatImage("", text)
                 listOf("meme").any { it in text } -> sendImage(memes.choose(), "meme")
                 react.any { it in text } -> send(quotes.choose())
                 members.any { it.key in text } -> {
@@ -152,15 +159,15 @@ class Bot : TelegramLongPollingBot() {
                     members[memberKey]?.let { send(it.random()) }
                 }
                 text == "yes" -> {
-                    send ("No")
+                    send("No")
                 }
                 text == "marion" -> {
-                    send ("Hello Marion")
+                    send("Hello Marion")
                 }
                 text == "memetest" -> {
-                    sendPostRequest(chatId, "","")
+                    sendPostRequest(chatId, "", "")
                 }
-                text == "no" -> send ("Yes")
+                text == "no" -> send("Yes")
                 hated != null -> send("Ich mag nicht ${hated}!")
                 loved != null -> send("Ich liebe ${loved}!")
                 listOf("joke", "witz").any { it in text } -> jokes.choose().let {
