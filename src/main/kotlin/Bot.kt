54f687a62c6f516806a5443b3600c988cf4d4938
Alexander Schamne <alexander.schamne@gmail.com>
2021-01-24 19:28:34 +0000

    parse respinse

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -106,7 +106,8 @@ class Bot : TelegramLongPollingBot() {
     "suit" to "922147",
     "whatif" to "61516",
     "sparta" to "195389",
-    "magic" to "32399536")
+    "magic" to "32399536",
+    "ned" to "18552174")
 
     fun sendImage(chatId: Long, template: String, text0: String, text1: String) = execute(SendPhoto().apply {
 
